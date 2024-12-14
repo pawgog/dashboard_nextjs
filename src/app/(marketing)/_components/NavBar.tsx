c59182ee71b717cd5f96551ef6c0b4a466134622
pawgog <pgogola10@gmail.com>
2024-12-14 14:37:43 +0100

    Create clerk provider and links

--- src/app/(marketing)/_components/NavBar.tsx
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -1,4 +1,5 @@
 import BrandLogo from "@/components/BrandLogo";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function NavBar() {
@@ -8,6 +9,23 @@ export default function NavBar() {
         <Link href="/" className="mr-auto">
           <BrandLogo />
         </Link>
+        <Link href="#" className="text-lg">
+          Pricing
+        </Link>
+        <Link href="#" className="text-lg">
+          Features
+        </Link>
+        <Link href="#" className="text-lg">
+          About
+        </Link>
+        <span className="text-lg">
+          <SignedIn>
+            <Link href="/dashboard">Dashboard</Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>Login</SignInButton>
+          </SignedOut>
+        </span>
       </nav>
     </header>
   );
