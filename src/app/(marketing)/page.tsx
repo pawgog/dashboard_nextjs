45989940cecfad8b33aff158457de6f38a308ca4
pawgog <pgogola10@gmail.com>
2024-12-16 20:06:32 +0100

    Add home page section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { SignUpButton } from "@clerk/nextjs";
+import { ArrowRightIcon } from "lucide-react";
+
 export default function Home() {
-  return <h1>Test</h1>;
+  return (
+    <>
+      <section className="min-h-screen bg-[radial-gradient(hsl(0,72%, 65%, 40%),hsl(24,62%,73%,40%),hsl(var(--background))_60%)] flex items-center justify-center text-center text-balance flex-col gap-8 px-4">
+        <h1 className="text-5xl lg:text-6xl xl:text-7xl font-bold tracking-tight m-4">
+          Lorem ipsum dolor sit amet
+        </h1>
+        <p className="text-lg lg:text-2xl max-w-screen-xl">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit earum fugiat
+          eligendi quidem nobis aut. At aperiam rerum ipsum mollitia
+          repellendus.
+        </p>
+        <SignUpButton>
+          <Button className="text-lg p-6 rounded-xl flex gap-2">
+            Let`s start for free <ArrowRightIcon className="size-5" />
+          </Button>
+        </SignUpButton>
+      </section>
+    </>
+  );
 }
