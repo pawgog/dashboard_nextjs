82c84657c9806e539ddbbe34a75ea940704d3aa5
pawgog <pgogola10@gmail.com>
2024-12-21 15:34:09 +0100

    Add subscription data

---
+++ src/app/data/subscriptionTiers.ts
@@ -0,0 +1,35 @@
+export const subscriptionTiers = {
+  Basic: {
+    name: "Basic",
+    price: 0,
+    maxNumberOfProducts: 1,
+    maxNumberOfVisits: 1000,
+    canAccessAnalytics: false,
+    canCustomizeBanner: false,
+    canRemoveBranding: false
+  },
+  Standard: {
+    name: "Standrad",
+    price: 4999,
+    maxNumberOfProducts: 5,
+    maxNumberOfVisits: 10000,
+    canAccessAnalytics: true,
+    canCustomizeBanner: false,
+    canRemoveBranding: true
+  },
+  Premium: {
+    name: "Premium",
+    price: 19999,
+    maxNumberOfProducts: 30,
+    maxNumberOfVisits: 100000,
+    canAccessAnalytics: true,
+    canCustomizeBanner: true,
+    canRemoveBranding: true
+  }
+} as const
+
+export const subscriptionTiersInOrder = [
+  subscriptionTiers.Basic,
+  subscriptionTiers.Standard,
+  subscriptionTiers.Premium
+] as const
