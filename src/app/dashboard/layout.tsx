a8a0c5fc37dfb02a0d2e6e14b5d8b90e073759bd
pawgog <pgogola10@gmail.com>
2025-01-03 21:40:28 +0100

    Create Clerk auth pages

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import NavBar from "./_components/NavBar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="bg-accent/5 min-h-screen">
+      <NavBar />
+      <div className="container py-6">{children}</div>
+    </div>
+  );
+}
