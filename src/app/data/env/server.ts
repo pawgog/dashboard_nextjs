fc3ddbb083efad69dab683c08acb0ea9eaeef342
pawgog <pgogola10@gmail.com>
2025-01-15 18:58:11 +0100

    Set up env type security process

---
+++ src/app/data/env/server.ts
@@ -0,0 +1,11 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  emptyStringAsUndefined: true,
+  server: {
+    CLERK_SECRET_KEY: z.string(),
+    DATABASE_URL: z.string().url()
+  },
+  experimental__runtimeEnv: process.env
+});
