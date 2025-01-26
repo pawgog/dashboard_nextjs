cc74966e6793d6ee6cf9523a037014ac3f5f4008
pawgog <pgogola10@gmail.com>
2025-01-26 14:56:55 +0100

    Create API webhooks route

---
+++ src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,48 @@
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { env } from '@/app/data/env/server'
+import { db } from '@/drizzle/db'
+import { UserSubscriptionTable } from '@/drizzle/schema'
+
+export async function POST(req: Request) {
+  const wh = new Webhook(env.CLERK_WEBHOOK_SECRET)
+
+  const headerPayload = await headers()
+  const svix_id = headerPayload.get('svix-id')
+  const svix_timestamp = headerPayload.get('svix-timestamp')
+  const svix_signature = headerPayload.get('svix-signature')
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error: Missing Svix headers', {
+      status: 400,
+    })
+  }
+
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
+
+  let event: WebhookEvent
+
+  try {
+    event = wh.verify(body, {
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
+    }) as WebhookEvent
+  } catch (err) {
+    console.error('Error: Could not verify webhook:', err)
+    return new Response('Error: Verification error', {
+      status: 400,
+    })
+  }
+
+  switch (event.type) {
+    case "user.created": {
+      await db.insert(UserSubscriptionTable).values({clerkUserId: event.data.id, tier: "Basic"});
+      break;
+    } 
+  }
+
+  return new Response('Webhook received', { status: 200 })
+}
