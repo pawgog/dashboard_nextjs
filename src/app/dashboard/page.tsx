3ca1841908f0893bad12d4eb02d70b73179a2f0f
pawgog <pgogola10@gmail.com>
2025-02-10 19:41:50 +0100

    Create get products function

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function Dashboard() {
+import { getProducts } from "@/server/db/products";
+import { auth } from "@clerk/nextjs/server";
+import { NoProducts } from "./_components/NoProducts";
+
+export default async function Dashboard() {
+  const { userId, redirectToSignIn } = await auth();
+  if (userId === null) return redirectToSignIn();
+
+  const products = await getProducts(userId, { limit: 6 });
+  if (products.length === 0) return <NoProducts />;
   return null;
 }
