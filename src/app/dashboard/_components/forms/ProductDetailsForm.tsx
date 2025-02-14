54eb617ad99266d85461a2a83a54d40954dfb6a0
pawgog <pgogola10@gmail.com>
2025-02-14 22:35:41 +0100

    Built default product form

--- src/app/dashboard/_components/forms/ProductDetailsForm.tsx
+++ src/app/dashboard/_components/forms/ProductDetailsForm.tsx
@@ -1,3 +1,53 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const productDetailsSchema = z.object({
+  name: z.string(),
+});
+
 export function ProductDetailsForm() {
-  return null;
+  const form = useForm<z.infer<typeof productDetailsSchema>>({
+    resolver: zodResolver(productDetailsSchema),
+  });
+
+  async function onSubmit(values: z.infer<typeof productDetailsSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex gap-6 flex-col"
+      >
+        <div className="grid gap-6 grid-cols-1 lg:grid-cols-2">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Product Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
+  );
 }
