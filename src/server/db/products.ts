c54dfb10b1da707952afeccc6d87bc9ba24f315a
pawgog <pgogola10@gmail.com>
2025-03-01 14:18:33 +0100

    Build cache functionality

--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,8 +1,15 @@
 import { db } from "@/drizzle/db";
 import { ProductCustomizationTable, ProductTable } from "@/drizzle/schema";
-import { eq } from "drizzle-orm";
+import { CACHE_TAGS, dbCache, getUserTag, revalidateDbCache } from "@/lib/cache";
+import { and, eq } from "drizzle-orm";
 
 export function getProducts(userId: string, { limit }: {limit?: number}) {
+  const cacheFn = dbCache(getProductsInternal, {tags: [getUserTag(userId, CACHE_TAGS.products)]})
+
+  return cacheFn(userId, { limit })
+}
+
+function getProductsInternal(userId: string, { limit }: {limit?: number}) {
   return db.query.ProductTable.findMany({
     where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
     orderBy: ({ createdAt }, { desc }) => desc(createdAt),
@@ -11,7 +18,7 @@ export function getProducts(userId: string, { limit }: {limit?: number}) {
 }
 
 export async function createProduct(data: typeof ProductTable.$inferInsert) {
-  const [newProduct] = await db.insert(ProductTable).values(data).returning({id: ProductTable.id});
+  const [newProduct] = await db.insert(ProductTable).values(data).returning({id: ProductTable.id, userId: ProductTable.clerkUserId});
 
   try {
     await db.insert(ProductCustomizationTable).values({
@@ -23,5 +30,30 @@ export async function createProduct(data: typeof ProductTable.$inferInsert) {
     await db.delete(ProductTable).where(eq(ProductTable.id, newProduct.id))
   }
 
+  revalidateDbCache({
+    tag: CACHE_TAGS.products,
+    userId: newProduct.userId,
+    id: newProduct.id,
+  })
+
   return newProduct;
+}
+
+export async function deleteProduct({ id, userId }: {
+  id: string
+  userId: string
+}) {
+  const { rowCount } = await db
+    .delete(ProductTable)
+    .where(and(eq(ProductTable.id, id), eq(ProductTable.clerkUserId, userId)))
+
+  if (rowCount > 0) {
+    revalidateDbCache({
+      tag: CACHE_TAGS.products,
+      userId,
+      id,
+    })
+  }
+
+  return rowCount > 0
 }
