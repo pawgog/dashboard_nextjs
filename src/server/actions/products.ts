361cdae2712d06691007e46921f7620bb36087b3
pawgog <pgogola10@gmail.com>
2025-03-01 14:18:06 +0100

    Add delete product action

--- src/server/actions/products.ts
+++ src/server/actions/products.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { redirect } from "next/navigation";
 import { productDetailsSchema } from "@/schemas/products";
 import { auth } from "@clerk/nextjs/server";
-import { createProduct as createProductDB } from "../db/products";
+import { createProduct as createProductDB, deleteProduct as deleteProductDB } from "../db/products";
 
 export async function createProduct(
   unsafeData: z.infer<typeof productDetailsSchema>
@@ -19,4 +19,20 @@ export async function createProduct(
   const { id } = await createProductDB({ ...data, clerkUserId: userId })
 
   redirect(`/dashboard/products/${id}/edit?tab=countries`)
+}
+
+export async function deleteProduct(id: string) {
+  const { userId } = await auth()
+  const errorMessage = "There was an error deleting your product"
+
+  if (userId == null) {
+    return { error: true, message: errorMessage }
+  }
+
+  const isSuccess = await deleteProductDB({ id, userId })
+
+  return {
+    error: !isSuccess,
+    message: isSuccess ? "Successfully deleted your product" : errorMessage,
+  }
 }
