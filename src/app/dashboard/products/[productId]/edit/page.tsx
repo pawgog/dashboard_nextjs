d9c9a9c7a4d77415bff081ab152388cac9fa92b8
pawgog <pgogola10@gmail.com>
2025-03-14 20:43:11 +0100

    Add country code form on page

--- src/app/dashboard/products/[productId]/edit/page.tsx
+++ src/app/dashboard/products/[productId]/edit/page.tsx
@@ -2,9 +2,16 @@ import { auth } from "@clerk/nextjs/server";
 import { notFound } from "next/navigation";
 import { ProductDetailsForm } from "@/app/dashboard/_components/forms/ProductDetailsForm";
 import { PageWithBackButton } from "@/app/dashboard/_components/PageWithBackButton";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { getProduct } from "@/server/db/products";
+import { getProduct, getProductCountryGroups } from "@/server/db/products";
+import { CountryDiscountsForm } from "@/app/dashboard/_components/forms/CountryDiscountForm";
 
 export default async function EditProductPage({
   params: { productId },
@@ -33,7 +40,9 @@ export default async function EditProductPage({
         <TabsContent value="details">
           <DetailsTab product={product} />
         </TabsContent>
-        <TabsContent value="countries">Countries</TabsContent>
+        <TabsContent value="countries">
+          <CountryTab productId={productId} userId={userId} />
+        </TabsContent>
         <TabsContent value="customization">Customization</TabsContent>
       </Tabs>
     </PageWithBackButton>
@@ -57,6 +66,37 @@ function DetailsTab({
       </CardHeader>
       <CardContent>
         <ProductDetailsForm product={product} />
+      </CardContent>
+    </Card>
+  );
+}
+
+async function CountryTab({
+  productId,
+  userId,
+}: {
+  productId: string;
+  userId: string;
+}) {
+  const countryGroups = await getProductCountryGroups({
+    productId,
+    userId,
+  });
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Country Discounts</CardTitle>
+        <CardDescription>
+          Leave the discount field blank if you do not want to display deals for
+          any specific parity group.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <CountryDiscountsForm
+          productId={productId}
+          countryGroups={countryGroups}
+        />
       </CardContent>
     </Card>
   );
