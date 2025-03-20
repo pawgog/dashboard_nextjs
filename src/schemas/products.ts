9986ec6bbc8d751e079bcf6c68a42b6ab87c7243
pawgog <pgogola10@gmail.com>
2025-03-20 21:23:15 +0100

    Create schema for product country code

--- src/schemas/products.ts
+++ src/schemas/products.ts
@@ -6,3 +6,29 @@ export const productDetailsSchema = z.object({
   url: z.string().url().min(1).transform(removeTrailingSlash),
   description: z.string().optional(),
 });
+
+export const productCountryDiscountsSchema = z.object({
+  groups: z.array(z.object({
+        countryGroupId: z.string().min(1, "Required"),
+        discountPercentage: z
+          .number()
+          .max(100)
+          .min(1)
+          .or(z.nan())
+          .transform(n => (isNaN(n) ? undefined : n))
+          .optional(),
+        coupon: z.string().optional(),
+      })
+      .refine(
+        value => {
+          const hasCoupon = value.coupon != null && value.coupon.length > 0
+          const hasDiscount = value.discountPercentage != null
+          return !(hasCoupon && !hasDiscount)
+        },
+        {
+          message: "A discount is required if a coupon code is provided",
+          path: ["root"],
+        }
+      )
+  ),
+})
