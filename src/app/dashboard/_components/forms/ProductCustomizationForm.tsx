e8c6775e193d6a67505cb30298e207fdfd70f9a7
pawgog <pgogola10@gmail.com>
2025-03-29 12:54:41 +0100

    Create product customization form

---
+++ src/app/dashboard/_components/forms/ProductCustomizationForm.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { RequiredLabelIcon } from "@/components/RequiredLabelIcon";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { productCustomizationSchema } from "@/schemas/products";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export function ProductCustomizationForm({
+  customization,
+  canCustomizeBanner,
+}: // canRemoveBranding,
+{
+  customization: {
+    productId: string;
+    locationMessage: string;
+    backgroundColor: string;
+    textColor: string;
+    fontSize: string;
+    bannerContainer: string;
+    isSticky: boolean;
+    classPrefix: string | null;
+  };
+  canRemoveBranding: boolean;
+  canCustomizeBanner: boolean;
+}) {
+  const form = useForm<z.infer<typeof productCustomizationSchema>>({
+    resolver: zodResolver(productCustomizationSchema),
+    defaultValues: {
+      ...customization,
+      classPrefix: customization.classPrefix ?? "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof productCustomizationSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <div>Banner</div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex gap-6 flex-col mt-8"
+        >
+          <div className="grid gap-8 grid-cols-1 md:grid-cols-2">
+            <FormField
+              control={form.control}
+              name="locationMessage"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Marketing App Discount Message
+                    <RequiredLabelIcon />
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      disabled={!canCustomizeBanner}
+                      className="min-h-20 resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    {"Data Parameters: {country}, {coupon}, {discount}"}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          {canCustomizeBanner && (
+            <div className="self-end">
+              <Button disabled={form.formState.isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          )}
+        </form>
+      </Form>
+    </>
+  );
+}
