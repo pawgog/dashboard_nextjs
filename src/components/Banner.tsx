b33585ee1922be56306bff86b734d7564e00d685
pawgog <pgogola10@gmail.com>
2025-04-02 21:33:13 +0200

    Create banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,83 @@
+import { env } from "@/app/data/env/client";
+
+export function Banner({
+  message,
+  mappings,
+  customization,
+  canRemoveBranding,
+}: {
+  canRemoveBranding: boolean;
+  message: string;
+  mappings: {
+    coupon: string;
+    discount: string;
+    country: string;
+  };
+  customization: {
+    backgroundColor: string;
+    textColor: string;
+    fontSize: string;
+    isSticky: boolean;
+    classPrefix?: string | null;
+  };
+}) {
+  const prefix = customization.classPrefix ?? "";
+  const mappedMessage = Object.entries(mappings).reduce(
+    (mappedMessage, [key, value]) => {
+      return mappedMessage.replace(new RegExp(`{${key}}`, "g"), value);
+    },
+    message.replace(/'/g, "&#39;")
+  );
+
+  return (
+    <>
+      <style type="text/css">
+        {`
+          .${prefix}marketing-app-container {
+            all: revert;
+            display: flex;
+            flex-direction: column;
+            gap: .5em;
+            background-color: ${customization.backgroundColor};
+            color: ${customization.textColor};
+            font-size: ${customization.fontSize};
+            font-family: inherit;
+            padding: 1rem;
+            ${customization.isSticky ? "position: sticky;" : ""}
+            left: 0;
+            right: 0;
+            top: 0;
+            text-wrap: balance;
+            text-align: center;
+          }
+
+          .${prefix}marketing-app-branding {
+            color: inherit;
+            font-size: inherit;
+            display: inline-block;
+            text-decoration: underline;
+          }
+        `}
+      </style>
+
+      <div
+        className={`${prefix}marketing-app-container ${prefix}marketing-app-override`}
+      >
+        <span
+          className={`${prefix}marketing-app-message ${prefix}marketing-app-override`}
+          dangerouslySetInnerHTML={{
+            __html: mappedMessage,
+          }}
+        />
+        {!canRemoveBranding && (
+          <a
+            className={`${prefix}marketing-app-branding`}
+            href={`${env.NEXT_PUBLIC_SERVER_URL}`}
+          >
+            Powered by Marketing App
+          </a>
+        )}
+      </div>
+    </>
+  );
+}
