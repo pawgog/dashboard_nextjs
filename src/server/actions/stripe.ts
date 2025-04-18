8a968f9cd12b13be8ab713d43d019e439ae99a11
pawgog <pgogola10@gmail.com>
2025-04-18 17:50:48 +0200

    Create missing create custom session in stripe

--- src/server/actions/stripe.ts
+++ src/server/actions/stripe.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { currentUser, User } from "@clerk/nextjs/server"
+import { auth, currentUser, User } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import { Stripe } from "stripe"
 
@@ -10,6 +10,54 @@ import { env as serverEnv } from "@/app/data/env/server"
 import { env as clientEnv } from "@/app/data/env/client"
 
 const stripe = new Stripe(serverEnv.STRIPE_SECRET_KEY)
+
+export async function createCancelSession() {
+  const user = await currentUser()
+  if (user == null) return { error: true }
+
+  const subscription = await getUserSubscription(user.id)
+
+  if (subscription == null) return { error: true }
+
+  if (
+    subscription.stripeCustomerId == null ||
+    subscription.stripeSubscriptionId == null
+  ) {
+    return new Response(null, { status: 500 })
+  }
+
+  const portalSession = await stripe.billingPortal.sessions.create({
+    customer: subscription.stripeCustomerId,
+    return_url: `${clientEnv.NEXT_PUBLIC_SERVER_URL}/dashboard/subscription`,
+    flow_data: {
+      type: "subscription_cancel",
+      subscription_cancel: {
+        subscription: subscription.stripeSubscriptionId,
+      },
+    },
+  })
+
+  redirect(portalSession.url)
+}
+
+export async function createCustomerPortalSession() {
+  const { userId } = await auth()
+
+  if (userId == null) return { error: true }
+
+  const subscription = await getUserSubscription(userId)
+
+  if (subscription?.stripeCustomerId == null) {
+    return { error: true }
+  }
+
+  const portalSession = await stripe.billingPortal.sessions.create({
+    customer: subscription.stripeCustomerId,
+    return_url: `${clientEnv.NEXT_PUBLIC_SERVER_URL}/dashboard/subscription`,
+  })
+
+  redirect(portalSession.url)
+}
 
 export async function createCheckoutSession(tier: PaidTierNames) {
   const user = await currentUser()
@@ -22,6 +70,9 @@ export async function createCheckoutSession(tier: PaidTierNames) {
   if (subscription.stripeCustomerId == null) {
     const url = await getCheckoutSession(tier, user)
     if (url == null) return { error: true }
+    redirect(url)
+  } else {
+    const url = await getSubscriptionUpgradeSession(tier, subscription)
     redirect(url)
   }
 }
@@ -46,4 +97,41 @@ async function getCheckoutSession(tier: PaidTierNames, user: User) {
   })
 
   return session.url
+}
+
+async function getSubscriptionUpgradeSession(
+  tier: PaidTierNames,
+  subscription: {
+    stripeCustomerId: string | null
+    stripeSubscriptionId: string | null
+    stripeSubscriptionItemId: string | null
+  }
+) {
+  if (
+    subscription.stripeCustomerId == null ||
+    subscription.stripeSubscriptionId == null ||
+    subscription.stripeSubscriptionItemId == null
+  ) {
+    throw new Error()
+  }
+
+  const portalSession = await stripe.billingPortal.sessions.create({
+    customer: subscription.stripeCustomerId,
+    return_url: `${clientEnv.NEXT_PUBLIC_SERVER_URL}/dashboard/subscription`,
+    flow_data: {
+      type: "subscription_update_confirm",
+      subscription_update_confirm: {
+        subscription: subscription.stripeSubscriptionId,
+        items: [
+          {
+            id: subscription.stripeSubscriptionItemId,
+            price: subscriptionTiers[tier].stripePriceId,
+            quantity: 1,
+          },
+        ],
+      },
+    },
+  })
+
+  return portalSession.url
 }
