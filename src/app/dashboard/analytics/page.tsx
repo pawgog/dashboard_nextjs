da16afaf199ac795bfd46e3c3c3dc1647de4a234
pawgog <pgogola10@gmail.com>
2025-05-07 17:18:46 +0200

    Create new analytics page route

---
+++ src/app/dashboard/analytics/page.tsx
@@ -0,0 +1,63 @@
+import { HasPermission } from "@/components/HasPermission";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  CHART_INTERVALS,
+  getViewsByCountryChartData,
+} from "@/server/db/productViews";
+import { canAccessAnalytics } from "@/server/permissions";
+import { auth } from "@clerk/nextjs/server";
+import { ViewsByCountryChart } from "../_components/charts/ViewsByCountryChart";
+
+export default async function AnalyticsPage({
+  searchParams,
+}: {
+  searchParams: {
+    interval?: string;
+    timezone?: string;
+    productId?: string;
+  };
+}) {
+  const { userId, redirectToSignIn } = await auth();
+  if (userId == null) return redirectToSignIn();
+
+  const searchParamsObject = await searchParams;
+
+  const interval =
+    CHART_INTERVALS[
+      searchParamsObject.interval as keyof typeof CHART_INTERVALS
+    ] ?? CHART_INTERVALS.last7Days;
+  const timezone = searchParamsObject.timezone || "UTC";
+  const productId = searchParamsObject.productId;
+
+  return (
+    <>
+      <HasPermission permission={canAccessAnalytics} renderFallback>
+        <div className="flex flex-col gap-8">
+          <ViewsByCountryCard
+            interval={interval}
+            timezone={timezone}
+            userId={userId}
+            productId={productId}
+          />
+        </div>
+      </HasPermission>
+    </>
+  );
+}
+
+async function ViewsByCountryCard(
+  props: Parameters<typeof getViewsByCountryChartData>[0]
+) {
+  const chartData = await getViewsByCountryChartData(props);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Visitors Per Country</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ViewsByCountryChart chartData={chartData} />
+      </CardContent>
+    </Card>
+  );
+}
