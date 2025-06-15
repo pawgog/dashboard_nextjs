5047f69e6335d67046ed7a88fd081d5e7b86b1ba
pawgog <pgogola10@gmail.com>
2025-06-15 12:26:42 +0200

    Create async function for timezone component

--- src/app/dashboard/_components/TimezoneDropdownMenuItem.tsx
+++ src/app/dashboard/_components/TimezoneDropdownMenuItem.tsx
@@ -2,17 +2,18 @@ import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { createURL } from "@/lib/utils";
 import Link from "next/link";
 
-export function TimezoneDropdownMenuItem({
+export async function TimezoneDropdownMenuItem({
   searchParams,
 }: {
   searchParams: Record<string, string>;
 }) {
   const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const searchParamsObject = await searchParams;
 
   return (
     <DropdownMenuItem asChild>
       <Link
-        href={createURL("/dashboard/analytics", searchParams, {
+        href={createURL("/dashboard/analytics", searchParamsObject, {
           timezone: userTimezone,
         })}
       >
